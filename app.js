a0200568c2e9878f285e0d8f3eeafa9cc332d049
VuKhoiGVM <nguyenvukhoi150402@gmail.com>
2023-12-02 17:57:56 +0700

    fix api get

--- app.js
+++ app.js
@@ -1,53 +1,51 @@
-fetch(`https://655045947d203ab6626da933.mockapi.io/:endpoint`)
-.then((response) => response.json())
-.then((data) => {
+fetch(`https://655045947d203ab6626da933.mockapi.io/stats`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    for (let i of data) {
+      let card = document.createElement("div");
+      card.classList.add("card");
 
+      let imgContainer = document.createElement("div");
+      imgContainer.classList.add("image-container");
 
-for (let i of data) {
-let card = document.createElement("div")
-card.classList.add("card")
+      let image = document.createElement("img");
+      image.setAttribute("src", i.img);
+      imgContainer.appendChild(image);
+      card.appendChild(imgContainer);
 
-let imgContainer = document.createElement("div")
-imgContainer.classList.add("image-container")
+      let con = document.createElement("div");
+      con.classList.add("container");
 
-let image = document.createElement("img")
-image.setAttribute("src", i.image)
-imgContainer.appendChild(image)
-card.appendChild(imgContainer)
+      let hang = document.createElement("div");
+      // con = <div class="container"></div>
 
-let con = document.createElement("div")
-con.classList.add("container")
+      let title = document.createElement("h5");
+      title.classList.add("product-name");
+      title.innerHTML = i.name;
+      con.appendChild(title);
 
-let hang = document.createElemnent("div")
-// con = <div class="container"></div>
+      let price = document.createElement("h5");
+      price.innerHTML = i.price;
+      con.appendChild(price);
 
-let title = document.createElement("h5")
-title.classList.add("product-name")
-title.innerHTML = i.name
-con.appendChild(title)
+      card.appendChild(con);
 
-let prices = document.createElement("h5")
-prices.innerHTML = i.prices
-con.appendChild(prices)
-
-card.appendChild(con)
-
-// h5.innerHTML =""
-document.getElementById("products").appendChild(card)
-}
-
-document.getElementById("search").addEventListener("click", () => {
-let searchInput = document.getElementById("search-input").value
-let cards = document.querySelectorAll(".card")
-let productsName = document.querySelectorAll(".product-name")
-
-productsName.forEach((item, index) =>{
-    if (item.innerText.includes(searchInput)) {
-        cards[index].classList.remove("hide")
-    } else {
-        cards[index].classList.add("hide")
+      // h5.innerHTML =""
+      document.getElementById("products").appendChild(card);
     }
-})
-})
 
-})
+    document.getElementById("search").addEventListener("click", () => {
+      let searchInput = document.getElementById("search-input").value;
+      let cards = document.querySelectorAll(".card");
+      let productsName = document.querySelectorAll(".product-name");
+
+      productsName.forEach((item, index) => {
+        if (item.innerText.includes(searchInput)) {
+          cards[index].classList.remove("hide");
+        } else {
+          cards[index].classList.add("hide");
+        }
+      });
+    });
+  });
